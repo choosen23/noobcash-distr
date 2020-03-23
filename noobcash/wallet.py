4c7f75a21ea0f5ee6c3919c891348a525c1f0536
choosen23 <filippou.michail@gmail.com>
2020-03-23 23:47:21 +0200

    rsa keys with example

--- noobcash/wallet.py
+++ noobcash/wallet.py
@@ -1,73 +1,106 @@
 import binascii
 
 import Crypto
-import Crypto.Random
+from Crypto import Random
 from Crypto.Hash import SHA
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
+from Crypto.Cipher import PKCS1_OAEP
 import random
 import hashlib
 import json
 from time import time
 from urllib.parse import urlparse
 from uuid import uuid4
+import base64
+import ast
 
+
+def generate_keys():
+    # RSA modulus length must be a multiple of 256 and >= 1024
+    modulus_length = 256*4  # use larger value in production
+    privatekey = RSA.generate(modulus_length, Random.new().read)
+    publickey = privatekey.publickey()
+    return privatekey, publickey
+
+
+def encrypt_message(a_message, publickey):
+    encryptor = PKCS1_OAEP.new(publickey)
+    encrypted = encryptor.encrypt(bytes(a_message, "utf8"))
+    # base64 encoded strings are database friendly
+    encoded_encrypted_msg = base64.b64encode(encrypted)
+    return encoded_encrypted_msg
+
+
+def decrypt_message(encoded_encrypted_msg, privatekey):
+
+    decryptor = PKCS1_OAEP.new(privatekey)
+    decoded_encrypted_msg = base64.b64decode(encoded_encrypted_msg)
+    decrypted = decryptor.decrypt(ast.literal_eval(str(decoded_encrypted_msg)))
+
+    # decoded_decrypted_msg = privatekey.decrypt(decoded_encrypted_msg)
+    return decrypted
 
 
 class wallet:
 
-	def __init__(self):
-		##set
+    def __init__(self):
+        # set
 
-		self.public_key = None
-		self.private_key = None
+        self.public_key = None
+        self.private_key = None
 
-		self.generate_wallet()
+        self.generate_wallet()
 
-		#self_address
+        # self_address
 
-		with open('./TXOexample/someTXOs.json', encoding='utf8') as trs:
-			trs = json.load(trs)
+        with open('./TXOexample/someTXOs.json', encoding='utf8') as trs:
+            trs = json.load(trs)
 
-		self.transactions = trs
+        self.transactions = trs
 
-		#self.transactions
+        # self.transactions
 
-	def generate_wallet(self):
-		""" Generates a pair of public/private key using RSA algorithm """ 
-  		# public = hashlib.pbkdf2_hmac('sha256', b'password', b'salt', 100000).hex()
-		# #randomly creates a public key
-		public = "%032x" % random.getrandbits(256)
-		print(public)
-		#using public key and sha256 we create private key
-		private = hashlib.sha256(public.encode('utf8')).hexdigest()
-		print(private)
-  		
-		self.public_key = public
-		self.private_key = private
+    def generate_wallet(self):
+        """ Generates a pair of public/private key using RSA algorithm """
 
-		print("New wallet is created.")
-		print("Wallet public key:", self.public_key)
-		print()
+        privatekey, publickey = generate_keys()
+        """ Example of use """
+        # message = "Oh nana Oh nanana"
+        # encrypted_msg = encrypt_message(message, publickey)
+        # decrypted_msg = decrypt_message(encrypted_msg, privatekey)
 
-	def balance(self):
-		""" Wallet balance is calculated by adding all UTXOs having this wallet as a reciever """
+        # print("%s " % (privatekey.exportKey()))
+        # print("%s " % (publickey.exportKey()))
+        # print(" Original content: %s " % (message))
+        # print("Encrypted message: %s " % (encrypted_msg))
+        # print("Decrypted message: %s " % (decrypted_msg))
 
-		acc_balance = 0
+        self.public_key = publickey
+        self.private_key = privatekey
 
-		for tr in self.transactions:
-			if tr['wallet_id'] == self.public_key and tr['type'] == 'UTXO':
-				acc_balance += tr['amount']
+        print("New wallet is created.")
+        print("Wallet public key:", self.public_key)
+        print()
 
-		return acc_balance
+    def balance(self):
+        """ Wallet balance is calculated by adding all UTXOs having this wallet as a reciever """
 
-	def showBalance(self):
-		print("Wallet public key:", self.public_key)
-		print("Balance:", self.balance(), "NBC")
-		print()
+        acc_balance = 0
+
+        for tr in self.transactions:
+            if tr['wallet_id'] == self.public_key and tr['type'] == 'UTXO':
+                acc_balance += tr['amount']
+
+        return acc_balance
+
+    def showBalance(self):
+        print("Wallet public key:", self.public_key)
+        print("Balance:", self.balance(), "NBC")
+        print()
 
 
 if __name__ == '__main__':
 
-	ros_wallet = wallet()
-	ros_wallet.showBalance()
+    ros_wallet = wallet()
+    ros_wallet.showBalance()
