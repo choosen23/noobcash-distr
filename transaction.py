3b05cddb4fa350dc99d1a84b1defe5c4f76fd1c8
Vxiros <xiros.vas@gmail.com>
2020-03-23 18:11:49 +0200

    com

--- transaction.py
+++ transaction.py
@@ -156,3 +156,10 @@ if __name__ == "__main__":
 	    trs = json.load(trs)
 
     transaction = Transaction("1", "privatekey2", "recipient3", 100, trs)
+
+    h = SHA.new(b'ss')
+    h2=h.hexdigest()
+    print(binascii.a2b_uu(str(h2)))
+
+    h.update(b'Hello')
+    print(h.hexdigest())
